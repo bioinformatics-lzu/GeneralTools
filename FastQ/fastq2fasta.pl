3d42783fe20960f87c5a36fc2eed63966d9ddc84
wk8910 <wk8910@gmail.com>
2014-11-23 18:53:19 +0800

    fix bug in fastq2fasta.pl

--- FastQ/fastq2fasta.pl
+++ FastQ/fastq2fasta.pl
@@ -9,19 +9,22 @@ die "Usage: $0 <fastq file> <fasta file>\n" if(!$fastq || !$fasta);
 
 open(I,"< $fastq")||die "Cannot open $fastq!\n";
 open(O,"> $fasta")||die "Cannot create $fasta!\n";
-my $light=0;
+my $count=0;
 while (<I>) {
-    if(/^\@(.*)/){
-        $light=1;
-        print O ">$1\n";
+    chomp;
+    if(/^\@(\S+)/){
+        my $name=$1;
+        print O ">$name\n";
+        $count=0;
         next;
     }
-    elsif(/^\+/){
-        $light=0;
+    if(/^\+$/){
+        while ($count-->0) {
+            <I>;
+        }
         next;
     }
-    if($light==1){
-        print O "$_";
-    }
+    print O "$_\n";
+    $count++;
 }
 close I;
