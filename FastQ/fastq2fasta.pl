deee80bccb3ce9b0dff8e96802835310b0897ee9
kunwang <wangkun@genedock.com>
2014-12-02 10:17:47 +0800

    Update fastq2fasta.pl
    small update

--- FastQ/fastq2fasta.pl
+++ FastQ/fastq2fasta.pl
@@ -1,11 +1,12 @@
 #! /usr/bin/env perl
+# transfer fastq file to fasta file, especially designed for consensus fq file from samtools.
 # author: wangkun
 use strict;
 use warnings;
 
 my ($fastq,$fasta)=@ARGV;
 
-die "Usage: $0 <fastq file> <fasta file>\n" if(!$fastq || !$fasta);
+die "Usage: $0 <fastq file> <fasta file>\n" if(@ARGV<2);
 
 open(I,"< $fastq")||die "Cannot open $fastq!\n";
 open(O,"> $fasta")||die "Cannot create $fasta!\n";
